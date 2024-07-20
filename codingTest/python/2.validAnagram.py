ce37681dcffb33e8b9767bdc593676375a8c1019
강규선(Kyusun Kang)/전시서비스개발/11ST <way1024@11stcorp.com>
2024-07-20 14:56:54 +0900

    study | leetCode

---
+++ codingTest/python/2.validAnagram.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if sorted(s) == sorted(t) :
+            return True
+        else :
+            return False
+        #if sort(s) == sort(t) :
+        #    return True
+        #else :
+        #    return False
+
